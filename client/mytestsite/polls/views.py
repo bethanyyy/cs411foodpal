65fef5c51417c133c28fe5f1c0aa3625df569e9e
Jiacheng Zhu <jzhu62@illinois.edu>
2018-03-16 16:47:27 -0500

    setup django

---
+++ client/mytestsite/polls/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+from django.shortcuts import render
+def home(request):
+    return render(request,'polls/home.html')
+
+
